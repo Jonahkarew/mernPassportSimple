27d3858ea55a185b7206121fc32fd684b983d936
Jonah Karew <jonahkarew@gmail.com>
2020-08-24 14:05:40 -0400

    app init, react comps, api routes, mongoose connection

---
+++ client/src/App.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import './App.css';
+import axios from "axios"
+function App() {
+
+  const [registerUsername, setRegisterUsername] = useState("")
+  const [registerPassword, setRegisterPassword] = useState("")
+  const [loginUsername, setLoginUsername] = useState("")
+  const [loginPassword, setLoginPassword] = useState("")
+
+
+const register = () => {
+  axios({
+    method: "post",
+    data: {
+      username: registerUsername,
+      password: registerPassword
+    },
+    withCredentials: true,
+    url: "/register"
+  }).then(res => console.log(res))
+}
+const login = () => {
+  axios({
+    method: "post",
+    data: {
+      username: loginUsername,
+      password: loginPassword
+    },
+    withCredentials: true,
+    url: "/login"
+  }).then(res => console.log(res))
+}
+const getUser = () => {
+  axios({
+    method: "get",
+    withCredentials: true,
+    url: "/getUser"
+  }).then(res => console.log(res))
+}
+
+
+
+
+
+
+  return (
+    <div className="App">
+
+
+      <div>
+        <h1>register</h1>
+        <input placeholder="username" onChange={e => setRegisterUsername(e.target.value)} />
+        <input placeholder="password" onChange={e => setRegisterPassword(e.target.value)}/>
+        <button onClick={register}>Submit</button>
+      </div>
+      <div>
+        <h1>Login</h1>
+        <input placeholder="username" onChange={e => setLoginUsername(e.target.value)} />
+        <input placeholder="password" onChange={e => setLoginUsername(e.target.value)} />
+        <button onClick={login}>Submit</button>
+      </div>
+     <div>
+       <h1>getUser</h1>
+       <button onClick={getUser}>Submit</button>
+     </div>
+      
+    </div>
+  );
+}
+
+export default App;
